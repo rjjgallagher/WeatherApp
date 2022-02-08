43e11fd0f49e4551b665852f44b39c92967a97df
Ryan Gallagher <97428947+rjjgallagher@users.noreply.github.com>
2022-02-08 16:06:18 -0600

    Added RecyclerView to display 16 days of weather dummy data.

---
+++ app/src/main/java/com/example/ryangallagher/ForecastTemp.kt
@@ -0,0 +1,11 @@
+package com.example.ryangallagher
+
+data class ForecastTemp(val day: Float, val max: Float, val min: Float)
+
+
+/*
+class ForecastTemp(dayVal: Float, maxVal: Float, minVal: Float) {
+    var day: Float = dayVal
+    var min: Float = minVal
+    var max: Float = maxVal
+}*/
--- app/src/main/java/com/example/ryangallagher/MainActivity.kt
+++ app/src/main/java/com/example/ryangallagher/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.ryangallagher
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun forecastActivity(view: android.view.View) {
+        val intent = Intent(this, ForecastActivity::class.java)
+        startActivity(intent)
+    }
+
+    /*
+    within the private class, he's got:
+    private lateinit var button: Button
+
+    then within the onCreate() function, he's got:
+    button.setOnClickListener {
+        startActivity(Intent(this, ForecastActivity::class.java))
+    }
+     */
+
 }
